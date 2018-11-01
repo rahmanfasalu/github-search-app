3ded3592e7e6c16dbb567d46da143e0927a693f2
rahmanfasalu <fasalukm@gmail.com>
2018-11-01 21:05:46 +0530

    perosnal access token commneted

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -10,7 +10,23 @@ export class JwtInterceptor implements HttpInterceptor {
        
                 request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
                 request = request.clone({ headers: request.headers.set('Accept', 'application/vnd.github.mercy-preview+json') });
-                request = request.clone({ headers: request.headers.set('Authorization', 'token af78fea730cd5451e85985bca359be748d6f85ba') });
+              
+              /* 
+                github is limited access, for test please replace the following accees token with your personal access token 
+                ie : replace : af78fea730cd5451e85985bca359be748d6f85ba  with your token 
+
+                for generating token go to 
+
+                https://github.com/settings/tokens/new
+
+                then check all permission, then generate token. 
+                copy the token and replace here. after that uncomment the following line  
+                */
+
+
+            // request = request.clone({ headers: request.headers.set('Authorization', 'token af78fea730cd5451e85985bca359be748d6f85ba') });  
+              
+              
         
         return next.handle(request);
     }
