57e2f100b668fa6dbeff1ad57da09f267a470f1f
Fasalu Rahman <fasalukm@gmail.com>
2020-09-18 12:31:57 +0200

    updated outdated packages

--- src/app/_services/repo.service.ts
+++ src/app/_services/repo.service.ts
@@ -1,70 +1,78 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { AppConfig } from '../_helpers/app.config';
-import { HttpErrorHandler, HandleError } from '../_helpers/http-error-handler.service';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { AppConfig } from "../_helpers/app.config";
+import {
+  HttpErrorHandler,
+  HandleError,
+} from "../_helpers/http-error-handler.service";
+import { map, catchError } from "rxjs/operators";
+import { Observable } from "rxjs";
+
 @Injectable()
 export class RepoService {
-
   private repoInfo = [];
   private handleError: HandleError;
   constructor(private http: HttpClient, httpErrorHandler: HttpErrorHandler) {
-    this.handleError = httpErrorHandler.createHandleError('RepoService');
+    this.handleError = httpErrorHandler.createHandleError("RepoService");
   }
 
   getAllRepos(query: string, page: string): Observable<any> {
     let pageSize = AppConfig.DATAPERPAGE;
     let url = environment.apiUrl + AppConfig.SERVICE.REPO_SEARCH;
     let obj = {
-      'per_page': pageSize,
-      'page': page,
-      'q': query
-    }
+      per_page: pageSize,
+      page: page,
+      q: query,
+    };
     let httpParams = new HttpParams({ fromObject: obj });
-    const options = { params: httpParams }
+    const options = { params: httpParams };
 
-    return this.http.get(url, options)
-      .pipe(map(response => {
-        this.repoInfo = this.repoInfo.concat(response && response['items'] || []);
+    return this.http.get(url, options).pipe(
+      map((response) => {
+        this.repoInfo = this.repoInfo.concat(
+          (response && response["items"]) || []
+        );
         return response;
       }),
-        catchError(this.handleError('getAllUsers', []))
+      catchError(this.handleError("getAllUsers", []))
     );
   }
 
-
   getRepoDetail(id) {
-    let arr =  this.repoInfo.filter(item => item.id === Number(id));
-    if(arr && arr.length){
+    let arr = this.repoInfo.filter((item) => item.id === Number(id));
+    if (arr && arr.length) {
       return arr[0];
-    }else{
+    } else {
       return {};
     }
   }
 
   clearRepoDetail() {
-    return this.repoInfo = [];
+    return (this.repoInfo = []);
   }
 
-  getRepoTabContent(url,owner,repo): Observable<any>{
-    return this.http.get(url)
-      .pipe(map(response => {
+  getRepoTabContent(url, owner, repo): Observable<any> {
+    return this.http.get(url).pipe(
+      map((response) => {
         return response;
       }),
-        catchError(this.handleError('getAllUsers', []))
+      catchError(this.handleError("getAllUsers", []))
     );
   }
- 
-  getSingleRepoDetails(owner:string,name:string): Observable<any> {
-    let url = environment.apiUrl + AppConfig.SERVICE.GET_SING_REPO_DETAILS.replace('##',owner).replace('$$',name); 
-    return this.http.get(url)
-      .pipe(map(response => {
-         return response;
+
+  getSingleRepoDetails(owner: string, name: string): Observable<any> {
+    let url =
+      environment.apiUrl +
+      AppConfig.SERVICE.GET_SING_REPO_DETAILS.replace("##", owner).replace(
+        "$$",
+        name
+      );
+    return this.http.get(url).pipe(
+      map((response) => {
+        return response;
       }),
-      catchError(this.handleError('getAllUsers', []))
+      catchError(this.handleError("getAllUsers", []))
     );
   }
-  
 }
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,66 +1,72 @@
-﻿import { UserObj } from './../_models/user';
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+﻿import { UserObj } from "./../_models/user";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
-import { environment } from '../../environments/environment';
-import { User } from '../_models';
-import { AppConfig } from '../_helpers/app.config';
-import { HttpErrorHandler, HandleError } from '../_helpers/http-error-handler.service';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { environment } from "../../environments/environment";
+import { User } from "../_models";
+import { AppConfig } from "../_helpers/app.config";
+import {
+  HttpErrorHandler,
+  HandleError,
+} from "../_helpers/http-error-handler.service";
+import { Observable } from "rxjs";
+import { catchError, map } from "rxjs/operators";
 
 @Injectable()
 export class UserService {
   private userInfo: UserObj[] = [];
   private handleError: HandleError;
   constructor(private http: HttpClient, httpErrorHandler: HttpErrorHandler) {
-    this.handleError = httpErrorHandler.createHandleError('UserService');
+    this.handleError = httpErrorHandler.createHandleError("UserService");
   }
 
   getAllUsers(query: string, page: string): Observable<any> {
     let url = environment.apiUrl + AppConfig.SERVICE.USER_SEARCH;
     let pageSize = AppConfig.DATAPERPAGE;
     let obj = {
-      'per_page': pageSize,
-      'page': page,
-      'q': query
-    }
+      per_page: pageSize,
+      page: page,
+      q: query,
+    };
     let httpParams = new HttpParams({ fromObject: obj });
     const options = query ? { params: httpParams } : {};
-    return this.http.get(url, options)
-      .pipe(map(response => {
-        this.userInfo = this.userInfo.concat(response && response['items'] || []);
+    return this.http.get(url, options).pipe(
+      map((response) => {
+        this.userInfo = this.userInfo.concat(
+          (response && response["items"]) || []
+        );
         return response;
       }),
-      catchError(this.handleError('getAllUsers', []))
+      catchError(this.handleError("getAllUsers", []))
     );
   }
 
   getUserDetail(login) {
-    return this.userInfo.filter(item => item.login === login);
+    return this.userInfo.filter((item) => item.login === login);
   }
 
   clearUserDetail() {
-    return this.userInfo = [];
+    return (this.userInfo = []);
   }
 
-  getSingleUserDetails(name:string): Observable<any> {
-    let url = environment.apiUrl + AppConfig.SERVICE.GET_USER_DETAILS.replace('##',name); 
-    return this.http.get(url)
-      .pipe(map(response => {
-         return response;
+  getSingleUserDetails(name: string): Observable<any> {
+    let url =
+      environment.apiUrl +
+      AppConfig.SERVICE.GET_USER_DETAILS.replace("##", name);
+    return this.http.get(url).pipe(
+      map((response) => {
+        return response;
       }),
-      catchError(this.handleError('getAllUsers', []))
+      catchError(this.handleError("getAllUsers", []))
     );
   }
 
-  getUserPageContent(url:string,name:string): Observable<any> {
-    return this.http.get(url)
-      .pipe(map(response => {
-         return response;
+  getUserPageContent(url: string, name: string): Observable<any> {
+    return this.http.get(url).pipe(
+      map((response) => {
+        return response;
       }),
-      catchError(this.handleError('getAllUsers', []))
+      catchError(this.handleError("getAllUsers", []))
     );
   }
-  
 }
